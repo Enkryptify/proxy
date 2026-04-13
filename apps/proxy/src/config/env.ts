3e830f31f905055900c26e9b9682c2770dfe1c2c
Axl Istas <axl.istas@student.vives.be>
2026-04-13 14:56:44 +0200

    t3 oss added

--- apps/proxy/src/config/env.ts
+++ apps/proxy/src/config/env.ts
@@ -1,1 +1,15 @@
-export {};
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    PORT: z
+      .string()
+      .transform((value) => parseInt(value, 10))
+      .pipe(z.number()),
+    DATABASE_URL: z.string(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
--- apps/proxy/src/plugins/db.ts
+++ apps/proxy/src/plugins/db.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "../lib/schemas/db";
+import { env } from "@/config/env";
+import * as schema from "@/lib/schemas/db";
 
-const client = postgres(Bun.env.DATABASE_URL!);
+const client = postgres(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
